28048a41145967d362b4a146799b6c0cc42cef3c
leeyebin0112 <81291095+leeyebin0112@users.noreply.github.com>
2021-08-09 02:08:17 +0900

    Create var.js

---
+++ 00.기본자바스크립트/module/var.js
@@ -0,0 +1,11 @@
+const odd = '홀수입니다.';
+const even = '짝수입니다.';
+
+module.exports = {
+    odd,
+    even,
+};    
+
+// exports.odd = '홀수입니다.';
+// exports.even = '짝수입니다.';
+// // 이 두줄에 코드로 정리할 수 있음.
