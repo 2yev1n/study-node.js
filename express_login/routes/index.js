93bc686e1a8b5934f9060ef42a0b02bc687c2f9a
yebin <yebin5127@gmail.com>
2021-09-15 08:30:58 +0900

    Correct routes

--- express_login/routes/index.js
+++ express_login/routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express")();
 const User = require("./user");
+const Mailer = require("./mail");
 
 router.use("/user", User);
+router.use("/user", Mailer);
 
 module.exports = router;
