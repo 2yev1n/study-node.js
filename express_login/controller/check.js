26c1d94175059c8b6cf381359c7c5cdd55e6bafb
yebin <yebin5127@gmail.com>
2021-09-29 22:06:57 +0900

    Add check.js

--- express_login/controller/check.js
+++ express_login/controller/check.js
@@ -1,14 +1,15 @@
 const router = require("../routes/check");
-const controller = require("../routes/mail");
+const tools = require("../routes/mail");
 
 const check = async(req, res) => {
     const { number } = req.body;
-
+    console.log(tools.authNum);
     try{
-        if(authNum === number) {
+        
+        if(tools.authNum === number) {
             res.status(200).json({
                 message: "인증번호 일치"
-            })
+            });
         } else {
             res.status(409).json({
                 message: "인증번호 불일치"
