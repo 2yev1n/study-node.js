b2ff7753adeb3ea129b7514c9a97ee88069640bc
yebin <yebin5127@gmail.com>
2021-09-30 21:51:56 +0900

    Update check.js

--- express_login/controller/check.js
+++ express_login/controller/check.js
@@ -1,25 +1,26 @@
-const router = require("../routes/check");
-const tools = require("../routes/mail");
+// const router = require("../routes/check");
+// const tools = require("../routes/mail");
 
-const check = async(req, res) => {
-    const { number } = req.body;
-    console.log(tools.authNum);
-    try{
-        
-        if(tools.authNum === number) {
-            res.status(200).json({
-                message: "인증번호 일치"
-            });
-        } else {
-            res.status(409).json({
-                message: "인증번호 불일치"
-            });
-        }
-    } catch(err) {
-        res.status(409).json({
-            messagse: "오류"
-        });
-    }
-};
+// const check = async(req, res) => {
+//     const { number } = req.body;
+//     const AuthNum = tools.authNum
+//     try{
+//         console.log(AuthNum);
+//         console.log(number);
+//         if(AuthNum === number) {
+//             res.status(200).json({
+//                 message: "인증번호 일치"
+//             });
+//         } else {
+//             res.status(409).json({
+//                 message: "인증번호 불일치"
+//             });
+//         }
+//     } catch(err) {
+//         res.status(409).json({
+//             messagse: "오류"
+//         });
+//     }
+// };
 
-module.exports = { check };
+// module.exports = { check };
