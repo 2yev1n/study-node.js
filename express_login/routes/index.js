fd29b5eede52d148106f89833306dd9fa3a57077
yebin <yebin5127@gmail.com>
2021-09-30 21:52:21 +0900

    Update index.js

--- express_login/routes/index.js
+++ express_login/routes/index.js
@@ -1,8 +1,8 @@
 const router = require("express")();
 const User = require("./user");
-const {mail} = require("./mail");
+const {Mail} = require("./mail");
 
 router.use("/user", User);
-router.use("/user", mail);
+router.use("/user", Mail);
 
 module.exports = router;
