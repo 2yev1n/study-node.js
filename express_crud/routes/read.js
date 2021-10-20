cb254ffa3eed58433674311ed1cefb07b27f0920
yebin <yebin5127@gmail.com>
2021-10-20 22:15:48 +0900

    Update routes/read

--- express_crud/routes/read.js
+++ express_crud/routes/read.js
@@ -2,6 +2,6 @@ const router = require("express")();
 const controller = require("../controllers/read");
 
 router.post("/readone", controller.readOne);
-router.post("/readall", controller.readAll);
+router.get("/readall", controller.readAll);
 
 module.exports = router;
