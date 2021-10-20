71073b124b7f79788dc5adcdb6d747624db330eb
yebin <yebin5127@gmail.com>
2021-10-20 20:08:07 +0900

    Update controllers

--- express_crud/controllers/create.js
+++ express_crud/controllers/create.js
@@ -1,25 +1,25 @@
-const user = require("../models/user");
-// const jwt = require("jsonwebtoken");
-const { User } = require("../models");
+// const user = require("../models/user");
+// // const jwt = require("jsonwebtoken");
+// const { User } = require("../models");
 
-const Create = async (req, res) => {
-    const { id, title, text } = req.body;
+// const Create = async (req, res) => {
+//     const { id, title, text } = req.body;
 
-    try{
-        await User.create({
-                id,
-                title,
-                text,
-            });
-        res.status(200).json({
-            message: "게시물 작성 성공",
-        });
-    } catch(err){
-        res.status(400).json({
-            message: "게시물 작성 실패"
-        });
-        console.error(err);
-    }
-};
+//     try{
+//         await User.create({
+//                 id,
+//                 title,
+//                 text,
+//             });
+//         res.status(200).json({
+//             message: "게시물 작성 성공",
+//         });
+//     } catch(err){
+//         res.status(400).json({
+//             message: "게시물 작성 실패"
+//         });
+//         console.error(err);
+//     }
+// };
 
-module.exports = { Create };
+// module.exports = { Create };
---
+++ express_crud/controllers/read.js
@@ -0,0 +1,33 @@
+const { User } = require("../models");
+
+const readOne = async(req, res) => {
+    const { id } = req.body;
+    try{
+        let user = await User.findOne({where : {id : id}});
+    
+        res.status(200).json(user);
+    } catch(err) {
+        res.status(404).json({
+            message: "해당 데이터 없음"
+        });
+        console.error(err);
+    }
+};
+
+const readAll = async(req, res) => {
+    try{
+        let users = await User.findAll();
+        res.status(200).json(users);
+    }
+    catch(err) {
+        res.status(404).json({
+            message:"오류"
+        });
+        console.error(err);
+    }
+};
+
+module.exports = { 
+    readOne,
+    readAll
+ };
