1970c8b9818208cca1e7ce7d2635081e68c5e9c7
yebin <yebin5127@gmail.com>
2021-10-22 21:03:21 +0900

    Add app.js

---
+++ multer/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const router = require("./controllers/user");
+const fs = require("fs");
+const PORT = process.env.PORT || 8000;
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+    console.log(PORT, " 번 포트에서 대기 중");
+})
