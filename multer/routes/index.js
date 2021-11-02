11694cba6136df66752fc8f1cb342d26ef711247
yebin <yebin5127@gmail.com>
2021-11-02 19:20:21 +0900

    Update multer/routes/index.js

--- multer/routes/index.js
+++ multer/routes/index.js
@@ -2,4 +2,16 @@ const app = require("express")();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
-app.post('/profile', upload.single("avatar"));
+app.post('/profile', upload.single("avatar"), function (req, res, next) {
+});
+
+app.post('/photos/upload', upload.array('photos', 12), function (req, res, next){    
+});
+
+const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1}, { name: 'gallery', maxCount: 8 }]);
+
+app.post('/cool-profile', cpUpload, function (req, res, next) {
+});
+
+app.post('/profile', upload.array(), function (req, res, next) {
+});
