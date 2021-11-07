60163faee2b54589d7cb66c1f971b5d466a6d8e3
yebin <yebin5127@gmail.com>
2021-11-07 15:48:41 +0900

    Update controllers/delete/deleteAll

--- express_crud/controllers/delete.js
+++ express_crud/controllers/delete.js
@@ -1,6 +1,6 @@
 const { User } = require("../models");
 
-const Delete = async(req, res) => {
+const deleteOne = async(req, res) => {
     const { id } = req.body;
     try{
         await User.destroy({ where : {id : id} });
@@ -8,6 +8,7 @@ const Delete = async(req, res) => {
         res.status(200).json({
             message: "삭제 완료"
         });
+
     } catch(err) {
         res.status(404).json({
             message: "id 없음"
@@ -16,6 +17,23 @@ const Delete = async(req, res) => {
     }
 };
 
+const deleteAll = async(req, res) => {
+    try{
+        await User.destroy({ where: {}, truncate: true });
+
+        res.status(200).json({
+            message: "전체삭제(초기화) 완료"
+        });
+
+    } catch (err) {
+        res.status(400).json({
+            message: "전체삭제 실패"
+        });
+        console.error(err);
+    }
+};
+
 module.exports = {
-    Delete
+    deleteOne,
+    deleteAll
 };
