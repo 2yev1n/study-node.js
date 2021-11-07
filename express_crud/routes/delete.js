0e31f11490e51e5fbbc8869c2f16b1a6f119a27a
yebin <yebin5127@gmail.com>
2021-11-07 15:49:10 +0900

    Update express_crud/routes/delete

--- express_crud/routes/delete.js
+++ express_crud/routes/delete.js
@@ -1,6 +1,7 @@
 const router = require("express")();
 const controller = require("../controllers/delete");
 
-router.post("/delete", controller.Delete);
+router.post("/deleteone", controller.deleteOne);
+router.get("/deleteall", controller.deleteAll);
 
 module.exports = router;
