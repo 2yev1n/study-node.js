024d9fcf52979ec1d509c370893336ed6cb6be0a
yebin <yebin5127@gmail.com>
2021-11-07 15:15:23 +0900

    Create multer/controllers/user

---
+++ multer/controllers/user.js
@@ -0,0 +1,22 @@
+const user = {
+    uploadProfile: async (req, res) => {
+        const image = req.file.path;
+        console.log(req.file);
+
+        try{
+            res.status(200).json({
+                message: " 요청 성공"
+            }, req.file);
+        }
+        catch(err) {
+            if (image === undefined) {
+            return res.status(400).json({
+                message: "이미자가 존재하지 않습니다."
+            });
+            console.error(err);
+            }
+        }
+    }
+};
+
+module.exports = user;
