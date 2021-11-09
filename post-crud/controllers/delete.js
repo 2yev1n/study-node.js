d24ac845bffd4f6ae84966ba3c5420336c3b04b5
yebin <yebin5127@gmail.com>
2021-11-09 22:30:50 +0900

    Create post-crud/controllers/delete

---
+++ post-crud/controllers/delete.js
@@ -0,0 +1,22 @@
+const { Post } = require("../models");
+
+const deletePost = async(req, res) => {
+    const { id, title } = req.body;
+
+    try{
+        await Post.destroy({ where : {id : id, title : title } });
+
+        res.status(200).json({
+            messgae: "게시물 삭제 성공"
+        });
+    } catch(err) {
+        res.status(404).json({
+            message: "잘못된 요청 양식"
+        });
+        console.error(err);
+    }
+};
+
+module.exports = {
+    deletePost
+};
