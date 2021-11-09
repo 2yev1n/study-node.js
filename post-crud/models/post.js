86ad317d46efd5032d709833ab6c958aa2b9a03c
yebin <yebin5127@gmail.com>
2021-11-09 22:31:57 +0900

    Update post-crud/models/post

--- post-crud/models/post.js
+++ post-crud/models/post.js
@@ -3,13 +3,14 @@ const sequelize = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('Post', {
         id : {
-            type: DataTypes.INTEGER(),
+            type: DataTypes.STRING(5),
             primaryKey: true,
-            allowNull: false,
+            allowNull: false
         },
         nickname : {
             type: DataTypes.STRING(45),
             foreignKey: true,
+            allowNull: true
         },
         content: {
             type: DataTypes.TEXT(),
@@ -20,8 +21,8 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: true,
         },
         picture: {
-            type: DataTypes.STRING(),
-            allowNull: false,
+            type: DataTypes.STRING(50),
+            allowNull: true
         },
         filed: {
             type: DataTypes.STRING(10),
