99e3e651c71a006a7352f4fc648081f4746d06ec
yebin <yebin5127@gmail.com>
2021-11-09 22:30:34 +0900

    Create post-crud/controllers/create

---
+++ post-crud/controllers/create.js
@@ -0,0 +1,26 @@
+const { Post } = require("../models");
+
+const createPost = async(req, res) => {
+    const { id, title, content, filed } = req.body;
+
+    try{
+        await Post.create({
+            id,
+            title,
+            content,
+            filed,
+        });
+        res.status(200).json({
+            message: "게시물 작성 성공"
+        });
+    } catch(err) {
+        res.status(404).json({
+            message: "작성 실패"
+        });
+        console.error(err);
+    }
+};
+
+module.exports = {
+    createPost
+};
