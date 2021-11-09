69eacf58c868f699f23e926c38c27b2abbb0db9b
yebin <yebin5127@gmail.com>
2021-11-09 13:29:06 +0900

    Update multer/routes

--- express_multer/routes/index.js
+++ express_multer/routes/index.js
@@ -1,28 +1,17 @@
-const express = require("express");
-const router = express();
+const router = require("express")();
 const multer = require("multer");
 const upload = multer({ dest: "uploadsFiles/" });   // dest : 파일이 저장될 위치
 const controller = require("../controllers/user");
 
-router.post('/profile', upload.single('acatar'), controller.uploadProfile);
-    // req.file은 'acatar'라는 필드의 파일 정보
-    // 텍스트 필드가 있는 경우, req.body가 이를 포함함.
+router.post('/upload', upload.single('userfile'), function(req, res){
+    res.send('Uploaded! : ', req.file); // object를 리턴함
+    console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
+  });
 
-// router.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
-//     // req.files는 'photos'라는 파일 정보를 배열형태로 가지고 있음.
-//     // 텍스트 필드가 있는 경우, req.body가 이를 포함함.
-// });
-
-// const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1}, { name: 'gallery', maxCount: 8}]);
-// router.post('/cool-profile', cpUpload, function (req, res, next) {
-//     // req.files는 (String -> Array) 형태의 객체
-//     // 필드명은 객체의 key에, 파일 정보는 배열로 value에 저장
-//     // 
-//     // ex)
-//     // req.files['avatar'][0] -> File
-//     // req.files['gallery'] -> Array
-//     //
-//     // 텍스트 필드가 있는 경우, req.body가 이를 포함함
-// });
+router.post('/upload', upload.single('avatar'), (req, res, next) => {
+    console.log(req.file);
+    console.log('파일 업로드');
+    res.redirect('/');
+});
 
 module.exports = router;
