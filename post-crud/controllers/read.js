3cec50e24b87e6f9d92e4c8ea0989a2d7ea3d8db
yebin <yebin5127@gmail.com>
2021-11-11 20:11:34 +0900

    Update post-crud/controllers/read

--- post-crud/controllers/read.js
+++ post-crud/controllers/read.js
@@ -1,29 +1,37 @@
 const { Post } = require("../models");
 
 const readAllPost = async(req, res) => {
-    const { id } = req.body;
+    const { writer } = req.body;
+
     try{
-        let post = await Post.findOne({ where : { id : id } });
+        let posts = await Post.findAll({
+             where : { writer : writer }
+            });
         
-        res.status(200).json(post);
+        res.status(200).json(posts);
     } catch (err) {
         res.status(404).json({
-            message: "해당 데이터 없음"
+            message: "데이터들 없음"
         });
         console.error(err);
     }
 };
 
 const readOnePost = async(req, res) => {
-    const { id, title } = req.body;
+    const { id } = req.body;
     
     try{ 
-        let post = await Post.findOne({ where : { id : id, title : title } });
-
+        let post = await Post.findOne({ where : { id : id } });
+        if( post === null)
+        {
+            res.status(404).json({
+                messgae: "해당 데이터 없음"
+            });
+        }
         res.status(200).json(post);
     } catch (err) {
         res.status(404).json({
-            messgae: "해당 데이터들 없음"
+            messgae: "해당 데이터 없음"
         });
         console.error(err);
     }
