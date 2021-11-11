f6f64609893a6a74c31899c1d44952f09599acbb
yebin <yebin5127@gmail.com>
2021-11-11 20:10:41 +0900

    Update post-crud/routes/index

--- express_multer/routes/index.js
+++ express_multer/routes/index.js
@@ -8,10 +8,10 @@ router.post('/upload', upload.single('userfile'), function(req, res){
     console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
   });
 
-router.post('/upload', upload.single('avatar'), (req, res, next) => {
-    console.log(req.file);
-    console.log('파일 업로드');
-    res.redirect('/');
-});
+// router.post('/upload', upload.single('avatar'), (req, res, next) => {
+//     console.log(req.file);
+//     console.log('파일 업로드');
+//     res.redirect('/');
+// });
 
 module.exports = router;
