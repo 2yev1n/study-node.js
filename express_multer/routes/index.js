3d30a987ca4f585c3f2e28a02847f550baac71da
yebin <yebin5127@gmail.com>
2021-11-27 21:49:25 +0900

    Update express_multer/routes/index

--- express_multer/routes/index.js
+++ express_multer/routes/index.js
@@ -1,17 +1,12 @@
-const router = require("express")();
+const express = require("express");
+const app = express();
+
 const multer = require("multer");
-const upload = multer({ dest: "uploadsFiles/" });   // dest : 파일이 저장될 위치
+const upload = multer({ 
+    dest: "uploads/" 
+});
 const controller = require("../controllers/user");
 
-router.post('/upload', upload.single('userfile'), function(req, res){
-    res.send('Uploaded! : ', req.file); // object를 리턴함
-    console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
-  });
+app.post('/profile', upload.single('image'), controller.uploadProfile);
 
-// router.post('/upload', upload.single('avatar'), (req, res, next) => {
-//     console.log(req.file);
-//     console.log('파일 업로드');
-//     res.redirect('/');
-// });
-
-module.exports = router;
+module.exports = app;
