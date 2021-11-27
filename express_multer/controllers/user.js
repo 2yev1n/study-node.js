db2b637e58344d4aa9f524eba229e7b9a4b34346
yebin <yebin5127@gmail.com>
2021-11-27 21:48:33 +0900

    Update express_multer/controllers/user

--- express_multer/controllers/user.js
+++ express_multer/controllers/user.js
@@ -1,22 +1,26 @@
+const multer = require("multer");
+const util = require("./util");
+
 const user = {
     uploadProfile: async (req, res) => {
         const image = req.file.path;
         console.log(req.file);
-
         try{
-            res.status(200).json({
-                message: " 요청 성공"
-            }, req.file);
+            if (image === undefined) {
+                return res.status(400).send(
+                    util.fail(400, "이미지가 존재하지 않습니다.")
+                )}
+            else await res.status(200).send(
+                util.success(200, "요청 성공", image)
+            );
         }
         catch(err) {
-            if (image === undefined) {
-            return res.status(400).json({
-                message: "이미지가 존재하지 않습니다."
-            }),
+            res.status(400).json({
+                message: "오류"
+            });
             console.error(err);
-            }
-        }
+            };
     }
 };
 
-module.exports = { user };
+module.exports = user;
