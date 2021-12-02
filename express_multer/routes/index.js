2245772fd3cfb5a04e50724705fce340a236526b
yebin <yebin5127@gmail.com>
2021-12-02 16:06:37 +0900

    Update express_multer/routes/index

--- express_multer/routes/index.js
+++ express_multer/routes/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 const multer = require("multer");
-const upload = multer({ 
-    dest: "uploads/" 
-});
 const controller = require("../controllers/user");
+
+const upload = multer({
+    storage: multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+      },
+      filename: (req, file, cb) => {
+        cb(null, new Date().valueOf() + path.extname(file.originalname));
+      },
+    }),
+  });
 
 app.post('/profile', upload.single('image'), controller.uploadProfile);
 
