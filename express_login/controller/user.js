79b44b65e0f09d453c58491d98624a4ccf384395
yebin <yebin5127@gmail.com>
2021-12-09 21:53:30 +0900

    Add hash password

--- express_login/controller/user.js
+++ express_login/controller/user.js
@@ -1,17 +1,24 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../models");
+const crypto = require("crypto");
+
+const salt = crypto.randomBytes(128).toString('base64');
 
 const sign_up = async(req, res) => {
     const {email, name, password} = req.body;   // 요청 받는 값들
     // const email = req.body.email;
     // const name = req.body.name; 
     // const password = req.body.password;
-    
+
+    const hashPassWord = crypto
+        .createHash('sha512')
+        .update(password + salt)
+        .digest('hex');
     try{
         await User.create({
                 email,
                 name,
-                password,
+                password : hashPassWord,
             });
         res.status(200).json({
             message: "성공"
@@ -20,6 +27,7 @@ const sign_up = async(req, res) => {
         res.status(409).json({
             message: "아이디 중복"
         });
+        console.error(err);
     }
 };
 
@@ -27,13 +35,17 @@ const login = async (req, res) => {
     const { email, password } = req.body;
     const secretkey = req.app.get("jwt-secret");
     console.log(email, password, secretkey);
+    const hashPassWord = crypto
+        .createHash('sha512')
+        .update(password + salt)
+        .digest('hex');
     try{
         const user = await User.findOne({
             where: {
                 email,
                 },
             });
-            if(user.password === password) {
+            if(user.password === hashPassWord) {
                 const accessToken = jwt.sign(
                     {
                         email : user.email,
