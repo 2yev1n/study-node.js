2c21bb71ac2d073f5a0e205a7cbe432e51ccfd53
yebin <yebin5127@gmail.com>
2021-12-22 14:00:43 +0900

    Create express_multer / middleware / upload

---
+++ express_multer/middleware/upload.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+const path = require("path")
+const s3Config = require("../config/s3.json");
+
+const s3 = new aws.S3(s3Config);
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'express-multer',
+        acl: 'public-read',
+        key: function(req, file, cb) {
+            cb(null, Date.now() + '.' + file.originalname.split('.').pop());
+        },
+        limit: {
+            fileSize: 1024 * 1024 * 20
+        },
+    }),
+});
+
+module.exports = upload;
