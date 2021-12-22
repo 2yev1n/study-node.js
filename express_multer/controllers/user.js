e79a930f99577618ddd39222a7e7408d35db8386
yebin <yebin5127@gmail.com>
2021-12-22 14:00:15 +0900

    Update express_multer s3 controllers

--- express_multer/controllers/user.js
+++ express_multer/controllers/user.js
@@ -1,26 +1,21 @@
-const multer = require("multer");
-const util = require("./util");
-
-const user = {
-    uploadProfile: async (req, res) => {
-        const image = req.file;
-        console.log(req.file);
-        try{
-            if (image === undefined) {
-                return res.status(400).send(
-                    util.fail(400, "이미지가 존재하지 않습니다.")
-                )}
-            else await res.status(200).send(
-                util.success(200, "요청 성공", image)
-            );
-        }
-        catch(err) {
-            res.status(400).json({
-                message: "오류"
-            });
-            console.error(err);
-            };
+const uploadImage = async(req, res) => {
+    const image = req.file
+        
+    try{ 
+        
+        res.status(200).json({
+            message: "사진 업로드 성공",
+        });
+    
+    } 
+    catch(err) {
+        res.status(403).json({
+           message: "사진 업로드 실패"
+        });
+        console.error(err);
     }
 };
 
-module.exports = user;
+module.exports = {
+    uploadImage
+};
