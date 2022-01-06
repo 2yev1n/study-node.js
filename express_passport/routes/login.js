3a6cd36c9003d47f139d4e07314acce2d453e314
yebin <yebin5127@gmail.com>
2022-01-07 02:13:09 +0900

    CREATE express_passport login routes

---
+++ express_passport/routes/login.js
@@ -0,0 +1,25 @@
+const router = require("express")();
+const { authenticate } = require("passport");
+const passport = require("passport");
+
+router.post("/login", passport, authenticate("local", {
+    failureRedirect: '/'
+}), (req, res) => {
+    res.redirect('/');
+});
+
+router.get("/auth/facebook", 
+    passport.authenticate("facebook", {
+        authType: "rerequest", 
+        cope: ["public_profile", "email"]
+    })
+);
+
+router.get("/auth/facebook/callback", 
+    passport.authenticate("facebook", {
+        failureRedirect: "/"
+    }),
+    function(req, res) {
+        res.redirect("/");
+    }
+);
