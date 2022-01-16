a3713793573bdc335209c15be7375c4925823034
yebin <yebin5127@gmail.com>
2022-01-17 00:02:07 +0900

    CREATE express_passport kakao passport

---
+++ express_passport/passport/kakao.js
@@ -0,0 +1,45 @@
+const passport = require("passport");
+const KakaoStrategy = require("passport-kakao").Strategy;
+const bcrypt = require("bcrypt");
+
+const { User } = require("../models");
+
+module.exports = () => {
+    passport.use(new KakaoStrategy({
+        clientID: process.env.KAKAO_ID,
+        callbackURL: "/auth/kakao/callback",
+    }, async(accessToken, refreshToken, profile, done) => {
+        console.log(profile);
+        console.log(accessToken);
+        console.log(refreshToken);
+
+        const hashPassword = bcrypt.hashSync(
+            JSON.stringify(profile.id), 10
+        );
+
+        console.log(hashPassword);
+            
+        try {
+            const exUser = await User.findOne({
+                where: {
+                    userID : profile.id,
+                    provider: 'kakao'
+                },
+            });
+            if (exUser) {
+                done(null, exUser);
+            } else if (!exUser) {
+                const newUser = await User.create({
+                    email: profile._json.kakao_account.email,
+                    userId: profile.id,
+                    provider: 'kakao',
+                    password: hashPassword
+                });
+                done(null, newUser);
+            }
+        } catch (err) {
+            console.error(err);
+            done(err);
+        };
+    }));
+};
