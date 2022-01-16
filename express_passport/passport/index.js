805792d8ff5d2711e28ee15706d48ca939faea08
yebin <yebin5127@gmail.com>
2022-01-17 00:02:45 +0900

    CREATE express_passport index passport

---
+++ express_passport/passport/index.js
@@ -0,0 +1,20 @@
+const passport = require("passport");
+const local = require("./local");
+const kakao = require("./kakao");
+const facebook = require("./facebook");
+
+const { User } = require("../models");
+
+module.exports = () => {
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+
+    passport.deserializeUser((user, done) => {
+        done(null, user);
+    });
+
+    local();
+    kakao();
+    facebook();
+};
