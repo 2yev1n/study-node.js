77600dabd1a99e4fc3132d386ee35c9b65043e7a
yebin <yebin5127@gmail.com>
2022-01-19 23:09:00 +0900

    FIX express_passport index passport

--- express_passport/passport/index.js
+++ express_passport/passport/index.js
@@ -7,7 +7,6 @@ const { User } = require("../models");
 
 module.exports = () => {
     passport.serializeUser((user, done) => {
-        console.log("dfa");
         return done(null, user);
     });
 
