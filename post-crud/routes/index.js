d270f5dfd3274214d49672d95cb300c3b8de034b
yebin <yebin5127@gmail.com>
2022-02-01 22:57:47 +0900

    UPDATE post-crud index route

--- post-crud/routes/index.js
+++ post-crud/routes/index.js
@@ -3,7 +3,8 @@ const createPost = require("./create");
 const deletePost = require("./delete");
 const readPost = require("./read");
 const updatePost = require("./update");
+const searchPost = require("./search");
 
-router.use("/post", createPost, deletePost, readPost, updatePost);
+router.use("/post", createPost, deletePost, readPost, updatePost, searchPost);
 
 module.exports = router;
