0f7cd82f82e258a3ca9832cf03590d8451837226
yebin <yebin5127@gmail.com>
2022-03-03 15:57:08 +0900

    UPDATE express_passport hashtag model

--- express_passport/models/hashtag.js
+++ express_passport/models/hashtag.js
@@ -1,18 +1,14 @@
 const Sequelize = require("sequelize");
 
-module.exports = (sequelize, Datatypes) => {
-    return sequelize.define("hashtag", {
-        title: {
-            type : Datatypes.STRING(15),
-            allowNull : false,
-            unique : true
-        },
-        postID: {
-            type: Datatypes.INTEGER,
+module.exports = class Hashtag extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            title: {
+            type: Sequelize.STRING(15),
             allowNull: false,
-        }
-    },
-    {
+            unique: true,
+            },
+        },  {
         sequelize,
         timestamps: true,
         modelName: 'Hashtag',
@@ -20,5 +16,10 @@ module.exports = (sequelize, Datatypes) => {
         paranoid: false,
         charset: 'UTF8MB4',
         collate: 'UTF8MB4_GENERAL_CI'
-    })
-}
+        })
+    }
+
+    static associate(db) {
+        db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag' });
+    }
+};
