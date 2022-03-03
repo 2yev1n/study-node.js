295d3eab4a5cf213b849337b35417786274e3804
yebin <yebin5127@gmail.com>
2022-03-03 15:56:56 +0900

    UPDATE express_passport post model

--- express_passport/models/post.js
+++ express_passport/models/post.js
@@ -1,33 +1,30 @@
 const Sequelize = require("sequelize");
 
-module.exports = (sequelize, Datatypes) => {
-    return sequelize.define("post", {
-        id: {
-            type: Datatypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
+module.exports = class Post extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            content: {
+                type: Sequelize.TEXT,
+                allowNull: false
+            },
+            img: {
+                type: Sequelize.STRING(200),
+                allowNull: true,
+            },
         },
-        content: {
-            type: Datatypes.TEXT,
-            allowNull: false
-        },
-        img: {
-            type: Datatypes.STRING(200),
-            allowNull: true,
-        },
-        UserID: {
-            type: Datatypes.INTEGER,
-            allowNull: false,
-        }
-    },
-    {
-        sequelize,
-        timestamps: true,
-        modelName: 'Post',
-        tableName: 'posts',
-        paranoid: false,
-        charset: 'UTF8MB4',
-        collate: 'UTF8MB4_GENERAL_CI'
-    })
-}
+        {
+            sequelize,
+            timestamps: true,
+            modelName: 'Post',
+            tableName: 'posts',
+            paranoid: false,
+            charset: 'UTF8MB4',
+            collate: 'UTF8MB4_GENERAL_CI'
+        });
+    }
+
+    static associate(db) {
+        db.Post.belongsTo(db.User);
+        db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
+    }
+};
