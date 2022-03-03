b662aa38d621be520353478c83c8f9e63ffb1918
yebin <yebin5127@gmail.com>
2022-03-03 15:56:45 +0900

    UPDATE express_passport index model

--- express_passport/models/index.js
+++ express_passport/models/index.js
@@ -1,38 +1,32 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
+const User = require("./user");
+const Post = require("./post");
+const Hashtag = require("./hashtag");
+
 
 const db = {};
-
 const sequelize = new Sequelize(
   config.database,
   config.username,
   config.password,
   config,
-)
+);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.User = require("./user")(sequelize, Sequelize);
-db.Post = require("./post")(sequelize, Sequelize);
-db.Hashtag = require("./hashtag")(sequelize, Sequelize);
+db.User = User;
+db.Post = Post;
+db.Hashtag = Hashtag;
 
-db.User.hasMany(db.Post, { foreignKey: "UserID", targetKey: "id" });
-db.Post.belongsTo(db.User, { foreignKey: "UserID" });
+User.init(sequelize);
+Post.init(sequelize);
+Hashtag.init(sequelize);
 
-db.Post.hasMany(db.Hashtag, { foreignKey: "postID", targetKey : "id" });
-db.Hashtag.belongsTo(db.Post, { foreignKey: "postID" });
-
-db.User.belongsToMany(db.User, {
-  foreignKey: 'followingId',
-  as: 'Followers',
-  through: 'follows',
-});
-db.User.belongsToMany(db.User, {
-  foreignKey: 'follwerld',
-  as: "Followings",
-  through: 'follows',
-});
+User.associate(db);
+Post.associate(db);
+Hashtag.associate(db);
 
 module.exports = db;
