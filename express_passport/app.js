8db1d477a3361e5e432cced7519b244d463e4167
yebin <yebin5127@gmail.com>
2022-03-04 09:28:40 +0900

    UPDATE express_passport app

--- express_passport/app.js
+++ express_passport/app.js
@@ -3,18 +3,35 @@ const morgan = require("morgan");
 const app = express();
 const path = require("path");
 const session = require("express-session");
+const nunjucks = require("nunjucks");
 const passport = require("passport");
 const passportConfig = require("./passport");
 const cookieParser = require("cookie-parser");
-const PORT = process.env.PORT||8080;
 
 const { sequelize } = require("./models");
 
 require("dotenv").config();
 
+const PORT = process.env.PORT||8080;
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log("데이터베이스 연결 성공");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+    passportConfig();
+
+const page = require("./routes/page");
 const auth = require("./routes/auth");
 const post = require("./routes/post");
-const page = require("./routes/page");
 const user = require("./routes/user");
 
 app.use(morgan('dev'));
@@ -38,27 +55,17 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use("/page", page);
+app.use("/", page);
 app.use("/post", post);
 app.use("/auth", auth);
 app.use("/user", user);
 
 
-app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 494;
-    next(error);
-})
-
-
-sequelize.sync({ force: false })
-    .then(() => {
-        console.log("데이터베이스 연결 성공");
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-    passportConfig();
+// app.use((req, res, next) => {
+//     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+//     error.status = 494;
+//     next(error);
+// })
 
 app.listen(PORT, () => {
     console.log(PORT , "번 포트에서 대기 중");
