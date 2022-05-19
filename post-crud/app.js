b066e17ed8a9e9b5b04fbf41e08e5c21c8a16d4b
yebin <yebin5127@gmail.com>
2022-05-19 15:52:33 +0900

    ADD post-crud app

--- post-crud/app.js
+++ post-crud/app.js
@@ -3,12 +3,16 @@ const app = express();
 const router = require("./routes");
 const { sequelize } = require("./models");
 const PORT = process.env.PORT || 3000;
-
+const morgan = require("morgan");
 
 require("dotenv").config();
 
+app.use(morgan('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
+
+app.set("jwt-secret", process.env.SECRET);
 
 app.use("/", router);
 
