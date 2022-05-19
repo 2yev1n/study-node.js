d1ca7bd88af836e566c23a70886fac4580ce0f92
yebin <yebin5127@gmail.com>
2022-05-19 15:54:02 +0900

    ADD post-crud user api

---
+++ post-crud/controllers/user.js
@@ -0,0 +1,63 @@
+const { User } = require("../models");
+const jwt = require("jsonwebtoken");
+
+const signUp = async(req, res) => {
+    const { name, email, password } = req.body;
+
+    try{
+        await User.create({
+            name,
+            email,
+            password
+        });
+        res.status(200).json({
+            message: "회원가입 성공",
+        });
+    } catch (err){
+        res.status(400).json({
+            message: "중복된 이메일",
+        });
+        console.error(err);
+    };
+};
+
+const login = async(req, res) => {
+    const { email, password } = req.body;
+    const secretKey = req.app.get("jwt-secret");
+
+    try{
+        const user = await User.findOne({
+            where : {
+                email : email
+            }
+        });
+        
+        if(user.password == password) {
+            const accessToken = jwt.sign({
+                id: id,
+                email: email,
+            },
+            secretKey,
+            {
+                expiresIn: "1h",
+            });
+
+            res.status(200).json({
+                message: "로그인 성공",
+                accessToken,
+            });
+        } else {
+            res.status(401).json({
+                message: "로그인 실패"
+            });
+            console.error(err);
+        }
+    } catch(err) {
+
+    };
+};
+
+module.exports = {
+    signUp,
+    login
+};
--- post-crud/routes/index.js
+++ post-crud/routes/index.js
@@ -4,7 +4,10 @@ const deletePost = require("./delete");
 const readPost = require("./read");
 const updatePost = require("./update");
 const searchPost = require("./search");
+const user = require("./user");
+const verifyToken = require("../middlewares/token");
 
-router.use("/post", createPost, deletePost, readPost, updatePost, searchPost);
+router.use("/user", user);
+router.use("/post", verifyToken, createPost, deletePost, readPost, updatePost, searchPost);
 
 module.exports = router;
