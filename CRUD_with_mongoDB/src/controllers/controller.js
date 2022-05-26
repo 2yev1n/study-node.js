405ddc147a0fd807f3ee5876e2950bc3cd783288
yebin <yebin5127@gmail.com>
2022-05-26 12:09:19 +0900

    FIX CRUD_with_mongoDB controller

--- CRUD_with_mongoDB/src/controllers/controller.js
+++ CRUD_with_mongoDB/src/controllers/controller.js
@@ -2,41 +2,42 @@ const db = require("../models/index");
 const Practice = db.practice;
 
 const read = async(req, res) => {
-    Practice.find().then((result) => {
+    
+    try{
+        const result = await Practice.find();
         
-        try{
-            res.status(200).json({
-                message: "조회 성공",
-                result
-            })
-        } catch(err) {
-            console.error(err);
-            return res.status(404).json({
-                message: "조회 실패"
-            });
-        };
-    });
+        res.status(200).json({
+            message: "조회 성공",
+            result
+        })
+    } catch(err) {
+        console.error(err);
+
+        return res.status(404).json({
+            message: "조회 실패"
+        });
+    };
+    ;
 };
 
-const create = async(req, res, next) => {
+const create = async(req, res) => {
     const practice = new Practice();
     
-    practice.title = req.body.title;
+    try{
+        practice.title = req.body.title;
+        practice.save(practice);
 
-    practice.save((err) => {
-        if(err) {
-            console.error(err);
-            return res.status(400).json({
-                message: "삽입 실패"
-            });
-        } else { 
-            return res.status(200).json({
-                message: "성공",
-                practice
-            })
-        }
-    })
-}
+        return res.status(200).json({
+            message: "작성 성공",
+            practice
+        });
+    } catch(err) { 
+        console.error(err);
+        return res.status(400).json({
+            message: "작성 실패",
+        });
+    };
+};
 
 module.exports = {
     read,
