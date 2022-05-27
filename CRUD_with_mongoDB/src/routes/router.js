7722531cc94a1a0ae2ba91b3e587c6f1742697b6
yebin <yebin5127@gmail.com>
2022-05-27 23:09:29 +0900

    ADD CRUD_with_mongoDB update api router

--- CRUD_with_mongoDB/src/routes/router.js
+++ CRUD_with_mongoDB/src/routes/router.js
@@ -4,5 +4,5 @@ const controller = require("../controllers/controller");
 
 router.get("/", controller.read);
 router.post("/", controller.create);
-
+router.patch("/:id", controller.update);
 module.exports = router;
