f1887a5ddd27ea9d7cf57ede64fe68afd214de18
yebin <yebin5127@gmail.com>
2022-06-08 23:39:19 +0900

    UPDATE express_graphQL schema

--- express_graphQL/graphql/schema.js
+++ express_graphQL/graphql/schema.js
@@ -1,48 +1,31 @@
 const { buildSchema } = require("graphql");
-const { Users } = require("../models");
 
 const schema = buildSchema(`
-    type Query{ 
-        hello: String!
-        world: String!
-        user: [User]!
+    type User {
+        id: Int
+        name: String
+        email: String
+        password: String
     }
 
-    type User {
-        id: Int!
-        name: String!
-        email: String!
-        password: String!
+    type Query{ 
+        hello: String
+        world: String
+        user(name: String): User
+        users: [User]
+    }
+
+    input UserInput{
+        email: String
+        name: String
+        password: String
+    }
+
+    type Mutation {
+        createUser(
+            userInput: UserInput
+        ): User!
     }
 `);
 
-const resolvers = {
-    hello: () => {
-        return 'Hello world!';
-    },
-    world: () => {
-        return '이건 조크';
-    },
-
-    Query: {
-        ViewUsers: async() => {
-            return await Users.all();
-        },
-        findUser: async(_, args) => {
-            return await Users.find({
-                where: args
-            });
-        },
-        createUser: async(_, args) => {
-            const newUser = await Users.create({
-                name: args.name,
-                email: args.email,
-                password: args.password,
-            });
-            return newUser;
-        },
-    },
-};
-
-
-module.exports = { schema, resolvers };
+module.exports = schema;
