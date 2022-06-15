a653846418800c9d196b708f2627855388ee23b1
yebin <yebin5127@gmail.com>
2022-06-15 17:12:31 +0900

    UPDATE CRUD_with_mongoDB post route

--- CRUD_with_mongoDB/src/routes/post.js
+++ CRUD_with_mongoDB/src/routes/post.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/controller");
+const router = express();
+const controller = require("../controllers/post");
 
 router.get("/", controller.readPost);
 router.post("/", controller.createPost);
