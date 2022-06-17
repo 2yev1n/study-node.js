b554677560799732e14e23c9beb4abd5ac415265
yebin <yebin5127@gmail.com>
2022-06-17 12:08:44 +0900

    UPDATE post-crud index route

--- post-crud/routes/index.js
+++ post-crud/routes/index.js
@@ -1,13 +1,8 @@
 const router = require("express")();
-const createPost = require("./create");
-const deletePost = require("./delete");
-const readPost = require("./read");
-const updatePost = require("./update");
-const searchPost = require("./search");
+const post = require("./post");
 const user = require("./user");
-const verifyToken = require("../middlewares/token");
 
 router.use("/user", user);
-router.use("/post", verifyToken, createPost, deletePost, readPost, updatePost, searchPost);
+router.use("/post", post);
 
 module.exports = router;
