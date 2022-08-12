8fb285f91c8592c7da1c47189f68fae0c3a2ccd7
yebin <yebin5127@gmail.com>
2022-08-12 13:14:33 +0900

    .

--- CRUD_with_mongoDB/src/routes/post.js
+++ CRUD_with_mongoDB/src/routes/post.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express();
 const controller = require("../controllers/post");
+const tokenMiddleware = require("../middleware/token");
 
 router.get("/", controller.readPost);
-router.post("/", controller.createPost);
+router.post("/", tokenMiddleware, controller.createPost);
 router.patch("/:id", controller.updatePost);
 router.delete("/:id", controller.deletePost);
 router.get("/:id", controller.readOnePost);
