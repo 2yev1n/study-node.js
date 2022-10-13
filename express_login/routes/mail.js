b7ef75ad432e8b462faa8381249cb937ecba427b
yebin <yebin5127@gmail.com>
2022-10-13 14:37:08 +0900

    UPDATE login routes

--- express_login/routes/mail.js
+++ express_login/routes/mail.js
@@ -1,51 +1,8 @@
 const router = require("express")();
-const mailer = require("../controller/mail");
+const controller = require("../controller/mail");
+const tool = require("../controller/check");
 
-let authNum;
+router.post("/mail", controller.sendMail);
+router.post("/mail/check", tool.check);
 
-router.post("/mail", (req, res) => {
-    const { email } = req.body;
-    
-    authNum = Math.random().toString().substr(2,6);
-
-    const emailParam = {
-        toEmail: email,
-        subject: "New Email",
-        text: authNum
-    };
-
-    mailer.sendGamil(emailParam);
-
-    res.status(200).send("이메일 발송 성공");
-    console.log(authNum);
-});
-
-const check = async(req, res) => {
-    const { number } = req.body;
-    try{
-        console.log(authNum);
-        console.log(number);
-        
-        if(authNum === number) {
-            res.status(200).json({
-                message: "인증번호 일치"
-            });
-        } else {
-            res.status(409).json({
-                message: "인증번호 불일치"
-            });
-        }
-    } catch(err) {
-        res.status(409).json({
-            messagse: "오류"
-        });
-    }
-};
-
-
-router.use("/mail", check);
-
-module.exports = {
-    Mail:router,
-    check
-};
+module.exports = router;
--- express_login/routes/user.js
+++ express_login/routes/user.js
@@ -1,6 +1,5 @@
 const router = require("express")();
 const controller = require("../controller/user");
-const middleware = require("../middleware/token");
 
 router.post("/sign_up", controller.sign_up);
 router.post("/login", controller.login);
