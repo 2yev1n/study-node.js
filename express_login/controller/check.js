8ddb6f616d2ad18bbc546a36423a6b26c5134654
yebin <yebin5127@gmail.com>
2022-10-13 14:36:48 +0900

    UPDATE login checking num api

--- express_login/controller/check.js
+++ express_login/controller/check.js
@@ -1,26 +1,32 @@
-// const router = require("../routes/check");
-// const tools = require("../routes/mail");
+const { User } = require("../models");
 
-// const check = async(req, res) => {
-//     const { number } = req.body;
-//     const AuthNum = tools.authNum
-//     try{
-//         console.log(AuthNum);
-//         console.log(number);
-//         if(AuthNum === number) {
-//             res.status(200).json({
-//                 message: "인증번호 일치"
-//             });
-//         } else {
-//             res.status(409).json({
-//                 message: "인증번호 불일치"
-//             });
-//         }
-//     } catch(err) {
-//         res.status(409).json({
-//             messagse: "오류"
-//         });
-//     }
-// };
+const check = async(req, res) => {
+    const { number } = req.body;
+    
+    try{
+        const checkingNum = User.findOne({
+            where: {
+                email
+            }
+        });
 
-// module.exports = { check };
+        console.log(checkingNum);
+        console.log(number);
+
+        if(checkingNum === number) {
+            res.status(200).json({
+                message: "인증번호 일치"
+            });
+        } else {
+            res.status(409).json({
+                message: "인증번호 불일치"
+            });
+        }
+    } catch(err) {
+        res.status(409).json({
+            messagse: "오류"
+        });
+    }
+};
+
+module.exports = { check };
